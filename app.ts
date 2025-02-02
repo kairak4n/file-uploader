fa022032aa3398ef65308d1df4ef0d4ef2686c5e
kairak4n <kshaunkaraca@gmail.com>
2025-02-02 15:14:45 +0300

    Add user authentication

--- app.ts
+++ app.ts
@@ -3,10 +3,14 @@ import session from 'express-session';
 import path from 'node:path';
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
 import { PrismaClient } from '@prisma/client';
-
+import authRouter from './routes/auth'
+import passport from 'passport';
+const LocalStrategy = require('passport-local').Strategy;
 require('dotenv').config()
+import bcrypt from 'bcryptjs'
 
 const app = express()
+const prisma = new PrismaClient()
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -29,8 +33,49 @@ app.use(session({
     ),
 }))
 
-app.get("/", (req: Request, res: Response) => {
-    res.send('you made it in')
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(
+    new LocalStrategy(async (username: string, password: string, done: any) => {
+        try{
+            const user = await prisma.user.findUnique({
+                where: {
+                    username
+                }
+            })
+            if (!user) {
+                return done(null, false, { message: "Username is incorrect" })
+            }
+            const match = bcrypt.compare(password, user.password)
+            if (!match) {
+                return done(null, false, { message: "Password is incorrect" })
+            }
+            return done(null, user)
+        } catch(e) {
+            return done(e)
+        } finally {
+            prisma.$disconnect()
+        }
+    })
+)
+
+passport.serializeUser((user: any, done) => {
+    done(null, user.id)
 })
+
+passport.deserializeUser(async (id: number, done) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            }
+        })
+        done(null, user)
+    } catch(err) {
+        done(err)
+    }
+})
+
+app.use(authRouter)
 
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
---
+++ routes/auth.ts
@@ -0,0 +1,46 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs'; 
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+
+const router = express.Router();
+const prisma = new PrismaClient()
+
+router.get('/', (req, res) => {
+    res.render('login')
+})
+    
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/',
+}))
+
+router.get('/sign-up', (req, res) => {
+    res.render('sign-up')
+})
+
+router.post('/sign-up',async (req, res) => {
+    try {
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(req.body.password, salt);
+        await prisma.user.create({
+            data: {
+                username: req.body.username,
+                password: hash,
+            }
+        })
+        res.redirect('/')
+    } catch (e) {
+        console.error(e)
+        res.status(500).send('internal')
+    } finally {
+        await prisma.$disconnect
+    }
+})
+
+export default router;
