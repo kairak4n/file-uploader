b69f09b228114a8eba9195ec7f93340293bacf0b
kairak4n <kshaunkaraca@gmail.com>
2025-02-04 12:48:18 +0300

    Add file downloading feature

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { PrismaClient } from '@prisma/client';
 import prisma from './database/prismaClient'
 import authRouter from './auth/auth.router';
 import filesRouter from './entities/files/files.router'
+import userRouter from './entities/user/user.router'
 import passport from 'passport';
 import localStrategy from './auth/strategies/local'
 require('dotenv').config()
@@ -55,5 +56,6 @@ passport.deserializeUser(async (id: number, done) => {
 
 app.use(authRouter)
 app.use(filesRouter)
+app.use("/user", userRouter)
 
 app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`))
